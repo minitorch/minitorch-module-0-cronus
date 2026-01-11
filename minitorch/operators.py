07817aa4863bbe6f9924e60b81d69f3424b24190
cronus <bocui552@gmail.com>
2026-01-11 10:26:23 +0800

    task 0.1 Operators

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -3,7 +3,6 @@
 import math
 
 # ## Task 0.1
-from typing import Callable, Iterable
 
 #
 # Implementation of a prelude of elementary functions.
@@ -34,14 +33,70 @@ from typing import Callable, Iterable
 
 # TODO: Implement for Task 0.1.
 def mul(x: float, y: float) -> float:
-    ...
+    return x * y
 
 
-def relu(x):
-    """
-    f(x) = x if x is grater than 0, else 0
-    """
-    ...
+def id(x: float) -> float:
+    return x
+
+
+def add(x: float, y: float) -> float:
+    return x + y
+
+
+def neg(x: float) -> float:
+    return -x
+
+
+def lt(x: float, y: float) -> bool:
+    return x < y
+
+
+def eq(x: float, y: float) -> bool:
+    return x == y
+
+
+def max(x: float, y: float) -> float:
+    return x if x > y else y
+
+
+def is_close(x: float) -> bool:
+    return True if math.fabs(x) < 1e-2 else False
+
+
+def sigmoid(x: float) -> float:
+    """f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}"""
+    return 1 / (1 + math.exp(-x)) if x >= 0 else math.exp(x) / (1 + math.exp(x))
+
+
+def relu(x: float):
+    """f(x) = x if x is grater than 0, else 0"""
+    return x if x > 0 else 0
+
+
+def log(x: float) -> float:
+    return math.log(x)
+
+
+def exp(x: float) -> float:
+    return math.exp(x)
+
+
+def inv(x: float) -> float:
+    return 1 / x
+
+
+def log_back(x: float, y: float) -> float:
+    return (1 / x) * y
+
+
+def inv_back(x: float, y: float) -> float:
+    return -(1 / x * x) * y
+
+
+def relu_back(x: float, y: float) -> float:
+    return y if x > 0 else 0
+
 
 # ## Task 0.3
 
